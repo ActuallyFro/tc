e697c38ff8177e634d210411cd80ce34cff647f0
ActuallyFro <ActuallyFro@github.com>
2016-08-08 00:27:53 -0400

    IO checks done

--- tc.sh
+++ tc.sh
@@ -1,10 +1,28 @@
+WorkingDir=`pwd`
 source ./include/functions.sh
 
-FileName=$1
-OutputType=$2
-OutputName=$3
+OType_1=$1
+IName_2=$2
+OName_3=$3
 
-#if [[ "$FileName" == "" ]]; then echo "No File Name provided!"; exit; fi
-if [ ! -f $FileName ]; then echo "File Does NOT exist!"; exit; fi
+OTypes=(pdf html pptx docx)
+
+#File Checks
+#===========
+#Output Type
+#-----------------
+if [[ "$OType_1" == "" ]]; then echo "No file type provided!"; exit; fi
+ValidType="false"
+for i in ${OTypes[@]}; do
+   if [[ "$i" == "$OType_1" ]]; then
+      ValidType="true"
+   fi
+done
+if [ "$ValidType" == "false" ]; then echo "File Output Type not supported!"; exit; fi
+
+#Input File EXISTS
+#-----------------
+if [[ "$IName_2" == "" ]]; then echo "File Does NOT exist!"; exit; fi
+if [ ! -f $IName_2 ]; then echo "File Does NOT exist!"; exit; fi
 
 #funtion_CheckEmpty($FileName)
